52dbcbb89400e37709ed80edc913d444f4569ebe
me <me.ncucu@kakaocorp.com>
2021-11-19 00:23:00 +0900

    [05] - 좋은 테스트의 FIRST 속성

--- [05]_좋은_테스트의_FIRST_속성/src/main/java/me/june/iloveyouboss/controller/QuestionController.java
+++ [05]_좋은_테스트의_FIRST_속성/src/main/java/me/june/iloveyouboss/controller/QuestionController.java
@@ -1,9 +1,12 @@
 package me.june.iloveyouboss.controller;
 
+import java.time.Clock;
 import me.june.iloveyouboss.domain.BooleanQuestion;
 import me.june.iloveyouboss.domain.Question;
 
 public class QuestionController {
+
+    private Clock clock = Clock.systemUTC();
 
     static Question q1 = new BooleanQuestion(1, "Tuition reimbursement?");
     static Question q2 = new BooleanQuestion(2, "Relocation package?");
@@ -15,4 +18,9 @@ public class QuestionController {
             return q2;
         }
     }
+
+    public void setClock(Clock clock) {
+        this.clock = clock;
+    }
+
 }
