c4435c1739607c9dac3f8036649ba2969d90199b
me <me.ncucu@kakaocorp.com>
2021-12-03 19:28:40 +0900

    [09] - 더 큰 설계 문제

--- [09]_더_큰_설계_문제/src/main/java/me/june/iloveyouboss/MatchSet.java
+++ [09]_더_큰_설계_문제/src/main/java/me/june/iloveyouboss/MatchSet.java
@@ -8,28 +8,60 @@ import java.util.Map;
 public class MatchSet {
 
     private Map<String, Answer> answers;
+    private Criteria criteria;
     private int score = 0;
 
     public MatchSet(Map<String, Answer> answers, Criteria criteria) {
         this.answers = answers;
-        this.score = score;
-        calculateScore(criteria);
+        this.criteria = criteria;
+        calculateScore();
     }
 
     public int getScore() {
         return score;
     }
 
+    public boolean matches() {
+        if (doesNotMeetAnyMustMatchCriterion()) {
+            return false;
+        }
+        return anyMatches();
+    }
+
     /**
      * 매칭되는 조건의 가중치를 합해서 점수를 계산
      */
-    private void calculateScore(Criteria criteria) {
+    private void calculateScore() {
         score = 0;
         for (Criterion criterion : criteria) {
             if (criterion.matches(answerMatching(criterion))) {
                 score += criterion.getWeight().getValue();
             }
         }
+    }
+
+    /**
+     * 필수 항목이 답변과 매칭되지 않으면 false 반환
+     */
+    private boolean doesNotMeetAnyMustMatchCriterion() {
+        for (Criterion criterion : criteria) {
+            boolean match = criterion.matches(answerMatching(criterion));
+            if (!match && criterion.getWeight() == Weight.MustMatch) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 필수 항목이 아니고 매칭되는것이 있다면 true, 아니라면 false 반환
+     */
+    private boolean anyMatches() {
+        boolean anyMatches = false;
+        for (Criterion criterion : criteria) {
+            anyMatches |= criterion.matches(answerMatching(criterion));
+        }
+        return anyMatches;
     }
 
     /**
