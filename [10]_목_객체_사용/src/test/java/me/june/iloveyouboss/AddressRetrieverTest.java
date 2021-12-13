f284f55d918f464e3eed5cfa9c10c6dfb7fd8605
me <me.ncucu@kakaocorp.com>
2021-12-13 23:02:37 +0900

    [10] - 목 객체 사용 : 테스트 스텁 - Mockito

--- [10]_목_객체_사용/src/test/java/me/june/iloveyouboss/AddressRetrieverTest.java
+++ [10]_목_객체_사용/src/test/java/me/june/iloveyouboss/AddressRetrieverTest.java
@@ -3,10 +3,22 @@ package me.june.iloveyouboss;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
 class AddressRetrieverTest {
+
+    private static final String MOCK_RESULT = "{\"address\":{"
+        + "\"house_number\":\"324\","
+        + "\"road\":\"North Tejon Street\","
+        + "\"city\":\"Colorado Springs\","
+        + "\"state\":\"Colorado\","
+        + "\"postcode\":\"80903\","
+        + "\"country_code\":\"us\"}"
+        + "}";
 
     /**
      * 람다를 활용한 스텁 구현
@@ -15,18 +27,13 @@ class AddressRetrieverTest {
         if (!url.contains("lat=38.000000&lon=-104.000000")) {
             fail("url" + url + " does not contains correct params");
         }
-        return "{\"address\":{"
-            + "\"house_number\":\"324\","
-            + "\"road\":\"North Tejon Street\","
-            + "\"city\":\"Colorado Springs\","
-            + "\"state\":\"Colorado\","
-            + "\"postcode\":\"80903\","
-            + "\"country_code\":\"us\"}"
-            + "}";
+        return MOCK_RESULT;
     };
 
     @Test
     void answerAppropriateAddressForValidCoordinates() throws Exception {
+        Http http = mock(Http.class);
+        when(http.get(contains("lat=38.000000&lon=-104.000000"))).thenReturn(MOCK_RESULT);
         AddressRetriever retriever = new AddressRetriever(http);
         Address address = retriever.retrieve(38.0, -104.0);
 
