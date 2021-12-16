37dc00c2cec27d9618cffbe867c272915d2e46df
me <me.ncucu@kakaocorp.com>
2021-12-17 00:10:06 +0900

    [11] - 테스트 리팩토링

---
+++ [11]_테스트_리팩토링/src/test/java/me/june/util/SearchTest.java
@@ -0,0 +1,63 @@
+package me.june.util;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.logging.Level;
+import org.junit.jupiter.api.Test;
+
+class SearchTest {
+
+    /**
+     * testSearch 는 무엇을 테스트하려는지 아무런 정보도 제공하지 않는다.
+     */
+    @Test
+    public void testSearch() {
+        try {
+            String pageContent = "There are certain queer times and occasions "
+                + "in this strange mixed affair we call life when a man "
+                + "takes this whole universe for a vast practical joke, "
+                + "though the wit thereof he but dimly discerns, and more "
+                + "than suspects that the joke is at nobody's expense but "
+                + "his own.";
+            byte[] bytes = pageContent.getBytes();
+            ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+            // search
+            Search search = new Search(stream, "practical joke", "1");
+            Search.LOGGER.setLevel(Level.OFF);
+            search.setSurroundingCharacterCount(10);
+            search.execute();
+            assertFalse(search.errored());
+            List<Match> matches = search.getMatches();
+            assertThat(matches, is(notNullValue()));
+            assertTrue(matches.size() >= 1);
+            Match match = matches.get(0);
+            assertThat(match.searchString, equalTo("practical joke"));
+            assertThat(match.surroundingContext,
+                equalTo("or a vast practical joke, though t"));
+            stream.close();
+
+            // negative
+            URLConnection connection =
+                new URL("http://bit.ly/15sYPA7").openConnection();
+            InputStream inputStream = connection.getInputStream();
+            search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
+            search.execute();
+            assertThat(search.getMatches().size(), equalTo(0));
+            stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("exception thrown in test" + e.getMessage());
+        }
+    }
+}
