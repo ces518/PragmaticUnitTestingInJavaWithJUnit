deaae02ff1fe21dff4d08d09045e96a865490230
me <me.ncucu@kakaocorp.com>
2021-12-21 00:26:01 +0900

    [13] - 까다로운 테스트 : 멀티스레드 코드 - 테스트를 위한 재설계

--- [13]_까다로운_테스트/src/main/java/me/june/iloveyouboss/ProfileMatcher.java
+++ [13]_까다로운_테스트/src/main/java/me/june/iloveyouboss/ProfileMatcher.java
@@ -5,13 +5,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 
 public class ProfileMatcher {
 
     private static final int DEFAULT_POOL_SIZE = 4;
+    private ExecutorService executors = Executors.newFixedThreadPool(DEFAULT_POOL_SIZE);
 
     private Map<String, Profile> profiles = new HashMap<>();
+
+    ExecutorService getExecutors() {
+        return executors;
+    }
 
     public void add(Profile profile) {
         profiles.put(profile.getId(), profile);
@@ -20,14 +26,21 @@ public class ProfileMatcher {
     /**
      * 멀티 스레드 코드
      */
-    public void findMatchingProfiles(Criteria criteria, MatchListener listener) {
-        ExecutorService executors = Executors.newFixedThreadPool(DEFAULT_POOL_SIZE);
-
-        for (MatchSet matchSet : collectMatchSets(criteria)) {
-            Runnable runnable = () -> process(listener, matchSet);
+    public void findMatchingProfiles(
+        Criteria criteria,
+        MatchListener listener,
+        List<MatchSet> matchSets,
+        BiConsumer<MatchListener, MatchSet> processFunction
+    ) {
+        for (MatchSet matchSet : matchSets) {
+            Runnable runnable = () -> processFunction.accept(listener, matchSet);
             executors.execute(runnable);
         }
         executors.shutdown();
+    }
+
+    public void findMatchProfiles(Criteria criteria, MatchListener listener) {
+        findMatchingProfiles(criteria, listener, collectMatchSets(criteria), this::process);
     }
 
     List<MatchSet> collectMatchSets(Criteria criteria) {
